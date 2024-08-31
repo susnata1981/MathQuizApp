c7144f771296e80f5c2d4e367c5366442c8ac3b3
Susnata <susnata@gmail.com>
2024-08-30 21:39:29 -0700

    Initial Commit

---
+++ MathQuiz/MathQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathQuizApp.swift
+//  MathQuiz
+//
+//  Created by Susnata Basak on 8/30/24.
+//
+
+import SwiftUI
+
+@main
+struct MathQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
