35fa6a718c105c9f30b35f39b3c77cd34473fdd4
Susnata <susnata@gmail.com>
2024-11-01 12:04:59 -0700

    Fixed StartQuizView and other styling changes.

---
+++ TestUI.swift
@@ -0,0 +1,18 @@
+//
+//  TestUI.swift
+//  MathQuiz
+//
+//  Created by Susnata Basak on 11/1/24.
+//
+
+import SwiftUI
+
+struct TestUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestUI()
+}
